4d43af6fff575e4f9e5f7d47c1213afd0d51f0bf
Syntax <gg.gamer9234@gmail.com>
2025-05-15 17:55:31 +0530

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,27 @@
+// utils.js
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+// Create and expose shuffled questions
+window.shuffledQuestions = (function() {
+  // Create a deep copy of original questions
+  const questionsCopy = JSON.parse(JSON.stringify(window.questions));
+  
+  // Shuffle question order
+  shuffleArray(questionsCopy);
+  
+  // Shuffle options for each question and update answer index
+  return questionsCopy.map(question => {
+    const correctAnswer = question.options[question.answer];
+    shuffleArray(question.options);
+    return {
+      ...question,
+      answer: question.options.indexOf(correctAnswer)
+    };
+  });
+})();
